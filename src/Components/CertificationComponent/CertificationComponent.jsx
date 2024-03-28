fbd1b86d7cb3a80c3b35870ccc0af30e45a7e488
AnetteStu <69764146+AnetteStu@users.noreply.github.com>
2024-03-28 20:02:23 +0100

    /about
    major improvements for achievements in about page

--- src/Components/CertificationComponent/CertificationComponent.jsx
+++ src/Components/CertificationComponent/CertificationComponent.jsx
@@ -1,21 +1,46 @@
 import { Link } from "react-router-dom"
 
+import style from '../../css/achievements.module.css'
+
 export default function CertificationComponent(data) {
   const url = data.description.toLowerCase().split(' ').join('_')
-  let size = 700
-  if(data.size === "small") {size=300}
-  if(data.size === "medium") {size=400}
+
   return (
-    <div className="certificateCard" style={{width:size+"px", height:size-60+"px", marginBottom:"1em"}}>
-      <Link 
-        to={`certificates/${url}`}
-        state={{data}}
-      > 
-        {/* <div className="row">
-          <h3>{data.name}</h3>
-        </div> */}
-        <img src={data.image} alt={data.name} style={{maxWidth:100+"%"}}/>
-      </Link>
-    </div>
+    <Link 
+      to={`certificates/${url}`}
+      state={{data}}
+    > 
+      <div className={style.certificate}>
+        <div className={style.certificate__info}>
+          <img src={data.image} alt={data.name} style={{maxHeight:100+"%"}}/>
+          <div className={style.certificate__href_credit}>
+            <h3>{data.name}</h3>
+            {/* <a href={data.credit} className={style.credit__link}>Credit</a> */}
+          </div>
+          <div className={style.certificate__description}>
+            <p>{data.description}</p>
+          </div>
+        </div>
+      </div>
+    </Link>
   )
 }
+
+
+
+
+
+// let size = 700
+// if(data.size === "small") {size=300}
+// if(data.size === "medium") {size=400}
+// <div className="certificateCard" style={{width:size+"px", height:size-60+"px", marginBottom:"1em"}}>
+//   <Link 
+//     to={`certificates/${url}`}
+//     state={{data}}
+//   > 
+//     {/* <div className="row">
+//       <h3>{data.name}</h3>
+//     </div> */}
+//     <img src={data.image} alt={data.name} style={{maxWidth:100+"%"}}/>
+//   </Link>
+// </div>
--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,12 +1,14 @@
 import Breadcrumb from "../../Components/Breadcrumb/Breadcrumb";
 import CertificationComponent from "../../Components/CertificationComponent/CertificationComponent";
+import style from '../../css/achievements.module.css'
+
 
 export default function About() {
   return (
     <div>
       <Breadcrumb/>
-      <h3>IT achievements</h3>
-      <div className="">
+      {/* <h3>IT achievements</h3> */}
+      {/* <div className="">
         <CertificationComponent
           name="Cisco Networking Fundementals"
           description="Certification of completed exam for the Cisco Networking Exam, free edition"
@@ -39,7 +41,37 @@ export default function About() {
           size="medium"
           credit="https://verify.w3schools.com/1N36QUNWGH"
         />
-      </div>
+      </div> */}
+
+      <section>
+        <div className={style.achievements}>
+          <CertificationComponent
+            name="Cisco Networking Fundementals"
+            description="Certification of completed exam for the Cisco Networking Exam, free edition"
+            image={process.env.PUBLIC_URL + "certifications/cisco/cisco-network_cert.png"}
+            credit="https://www.credly.com/badges/3b5f2aba-f731-46ca-8499-38b9c0d11612/public_url"
+          />
+          <CertificationComponent
+            name="Microsoft Fundamentals"
+            description="Certification of completed exam for the Microsoft Fundamentals"
+            image={process.env.PUBLIC_URL + "certifications/ms-900/ms-900_cert.png"}
+            credit="https://learn.microsoft.com/en-gb/users/anettedahle-9144/credentials/69b9d7db1ac59417"
+          />
+          <CertificationComponent 
+            name="HTML"
+            description="Completed the HTML Exam from W3Schools"
+            image={process.env.PUBLIC_URL + "certifications/html/html_cert.jpg"}
+            credit="https://verify.w3schools.com/1N5ANMYCGZ"
+          />
+          <CertificationComponent 
+            name="JavaScript"
+            description="Completed the JavaScript Exam from W3Schools"
+            image={process.env.PUBLIC_URL + "certifications/js/js_cert.jpg"}
+            credit="https://verify.w3schools.com/1N36QUNWGH"
+          />
+        </div>
+      </section>
+
     </div>
   )
 }
