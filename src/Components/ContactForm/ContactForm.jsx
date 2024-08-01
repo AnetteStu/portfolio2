f95abb611a0bd0d8071142b7a37cc8178bdb4d2d
AnetteStu <69764146+AnetteStu@users.noreply.github.com>
2024-08-01 02:21:04 +0200

    ContactForm and styles

--- src/Components/ContactForm/ContactForm.jsx
+++ src/Components/ContactForm/ContactForm.jsx
@@ -21,7 +21,6 @@ export default function ContactForm() {
       });
       const result = await response.json();
       if (result.success) {
-          console.log(result);
           navigate(`/contactsuccess`, {state:{result}})
       }
   }
--- src/Pages/ContactSuccess/ContactSuccess.jsx
+++ src/Pages/ContactSuccess/ContactSuccess.jsx
@@ -1,21 +1,29 @@
-import React from 'react'
-import { useLocation, useNavigate } from 'react-router-dom'
+import { useEffect } from 'react'
+import { Link, useLocation, useNavigate } from 'react-router-dom'
 
 export default function ContactSuccess() {
-  // const navigate = useNavigate();
-  const state = useLocation()
-  console.log(state);
-  const result = state
-  console.log(result.state.result);
-  // if(result.success != true) {
-  //   navigate("/")
-  // }
+  const navigate = useNavigate();
+  const content = useLocation()
+  console.log(content);
+
+  // useEffect(() => {
+  //   if(content.state === null) {
+  //     navigate("/")
+  //     return
+  //   }
+  // }, [])
+
+  const result = content.state.result
   return (
     <div>
       <h1>
-        {result.state.result.message}
+        {result.message}
       </h1>
-      <p>You will hear back from me shortly {result.state.result.data.name}.</p>
+      <p>You will hear back from me shortly {result.data.name}!</p>
+      <sub>Please give me a few days to answer. I have a full time job and a busy private life, but I check my email every day.</sub>
+      <button>
+        <Link to="/">Go back to home</Link>
+      </button>
     </div>
   )
 }
